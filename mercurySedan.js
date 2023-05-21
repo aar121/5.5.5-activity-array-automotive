191f38e8b7dd1502ee10e63acbeba096c17d8080
aar121 <arifrahim12345@gmail.com>
2023-05-21 10:06:34 -0400

    updated class information

--- mercurySedan.js
+++ mercurySedan.js
@@ -13,7 +13,42 @@ console.log(v.make)
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
-
+class MercurySedan {
+    constructor(make, model, year, color, mileage) {
+      super(make, model, year, color, mileage)
+      this.maximumPassengers = 5
+      this.passengers = 0
+      this.numberOfWheels = 4
+      this.maximumSpeed = 160
+      this.fuel = 10
+      this.scheduleService = false
+    }
+    loadPassenger(num) {
+        if (this.passengers < this.maximumPassengers) {
+            console.log("there is more space on this car")
+        }
+        else if (this.passengers = this.maximumPassengers) {
+            console.log("the car has reached maximum capacity")
+        }
+        else if (this.passengers > this.maximumPassengers) {
+            console.log("You won't be able to travel with so many passengers!")
+        }
+    }
+    start() {
+        if (this.fuel > 0) {
+            return this.start = true;
+        }
+    }
+    scheduleService(mileage) {
+        if (this.mileage > 30000) {
+            return this.scheduleService = true
+        }
+        else if (this.mileage < 30000) {
+            console.log("Your car doesn't need any maintenance at the moment")
+        }
+    }
+}
+    
 
 
 
